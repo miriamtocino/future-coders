812c5b198179abd9209f008226665bc55879e474
Miriam Tocino <miriam.tocino@gmail.com>
2017-10-17 10:13:34 +0200

    (Test) Add a couple of bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Grid, Jumbotron } from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+import Results from './components/Results';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Jumbotron>
+          <Grid>
+            <h1>Future Coders</h1>
+            <p>This is the homepage.</p>
+            <SearchForm />
+          </Grid>
+        </Jumbotron>
+        <Results />
       </div>
     );
   }
